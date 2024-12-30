4e70ef8516f15bbdd889593a8fdf2929aebc4fb2
Akhilsai Chidura <akhilsai1200@gmail.com>
2024-12-30 22:45:46 +0530

    upto Controller

---
+++ Services/Basket/Basket.Application/Handlers/GetBasketByUserNameHandler.cs
@@ -0,0 +1,25 @@
+﻿using Basket.Application.Mappers;
+using Basket.Application.Queries;
+using Basket.Application.Responses;
+using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Handlers
+{
+    public class GetBasketByUserNameHandler : IRequestHandler<GetBasketByUserNameQuery, ShoppingCartResponse>
+    {
+        private readonly IBasketRepository _BasketRepository;
+        public GetBasketByUserNameHandler(IBasketRepository basketRepository)
+        {
+            _BasketRepository = basketRepository;
+        }
+
+
+        public async Task<ShoppingCartResponse> Handle(GetBasketByUserNameQuery request, CancellationToken cancellationToken)
+        {
+            var shoppingCart = await _BasketRepository.GetBasket(request.UserName);
+            var shoppingCartResponse = BasketMapper.Mapper.Map<ShoppingCartResponse>(shoppingCart);
+            return shoppingCartResponse;
+        }
+    }
+}
